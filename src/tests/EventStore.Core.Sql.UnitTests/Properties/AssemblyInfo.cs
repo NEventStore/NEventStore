3f423d5737bf478595b82107ddf26fd44c7e0b9b
Oliver, Jonathan <joliver@localhost>
2010-06-24 10:50:59 -0600

    Added test project.

---
+++ src/tests/EventStore.Core.Sql.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EventStore.Core.Sql.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: Guid("091b6b55-7b48-43fa-b755-cf7adf7de528")]
