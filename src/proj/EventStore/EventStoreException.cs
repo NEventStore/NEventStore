de7dcce386791562655c64d05a7c4ab44122c2a5
Oliver, Jonathan <joliver@localhost>
2010-06-24 21:03:13 -0600

    Finished the public API XML docs.

--- src/proj/EventStore/EventStoreException.cs
+++ src/proj/EventStore/EventStoreException.cs
@@ -3,22 +3,42 @@ namespace EventStore
 	using System;
 	using System.Runtime.Serialization;
 
+	/// <summary>
+	/// Represents a general failure of the event store.
+	/// </summary>
 	public class EventStoreException : Exception
 	{
+		/// <summary>
+		/// Initializes a new instance of the EventStoreException class.
+		/// </summary>
 		public EventStoreException()
 		{
 		}
 
+		/// <summary>
+		/// Initializes a new instance of the EventStoreException class.
+		/// </summary>
+		/// <param name="message">The message that describes the error.</param>
 		public EventStoreException(string message)
 			: base(message)
 		{
 		}
 
+		/// <summary>
+		/// Initializes a new instance of the EventStoreException class.
+		/// </summary>
+		/// <param name="message">The message that describes the error.</param>
+		/// <param name="innerException">The message that is the cause of the current exception.</param>
 		public EventStoreException(string message, Exception innerException)
 			: base(message, innerException)
 		{
 		}
 
+		/// <summary>
+		/// Initializes a new instance of the EventStoreException class.
+		/// </summary>
+		/// <param name="info">The SerializationInfo that holds the serialized object data of the exception being thrown.</param>
+		/// <param name="context">The StreamingContext that contains contextual information about the source or destination.</param>
 		protected EventStoreException(SerializationInfo info, StreamingContext context)
 			: base(info, context)
 		{
