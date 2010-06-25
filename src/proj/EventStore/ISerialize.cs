b0274f535b6ae5c34e7f4fbccf9b533bff5805b6
Oliver, Jonathan <joliver@localhost>
2010-06-24 20:58:08 -0600

    Added XML API documentation.

--- src/proj/EventStore/ISerialize.cs
+++ src/proj/EventStore/ISerialize.cs
@@ -1,23 +1,23 @@
 namespace EventStore
 {
 	/// <summary>
-	/// 
+	/// Provides the ability to serialize and deserialize an object graph.
 	/// </summary>
 	public interface ISerialize
 	{
 		/// <summary>
-		/// 
+		/// Serializes the object graph provided.
 		/// </summary>
-		/// <param name="graph"></param>
-		/// <returns></returns>
+		/// <param name="graph">The object graph to be serialized.</param>
+		/// <returns>The serialized or byte representation of the serialized object.</returns>
 		byte[] Serialize(object graph);
 
 		/// <summary>
-		/// 
+		/// Deserializes the bytes provided.
 		/// </summary>
-		/// <typeparam name="T"></typeparam>
-		/// <param name="input"></param>
-		/// <returns></returns>
+		/// <typeparam name="T">The type of object to reconstruct.</typeparam>
+		/// <param name="input">The bytes from which the object will be reconstructed.</param>
+		/// <returns>The reconstructed object.</returns>
 		T Deserialize<T>(byte[] input);
 	}
 }
--- src/proj/EventStore/IStoreEvents.cs
+++ src/proj/EventStore/IStoreEvents.cs
@@ -5,17 +5,17 @@ namespace EventStore
 	using System.Collections.Generic;
 
 	/// <summary>
-	/// 
+	/// Provides the ability to save and retrieve objects which utilize event sourcing.
 	/// </summary>
 	public interface IStoreEvents
 	{
 		/// <summary>
-		/// 
+		/// Loads all events for the specified aggregate starting at the indicated version.
 		/// </summary>
-		/// <typeparam name="T"></typeparam>
-		/// <param name="id"></param>
-		/// <param name="startingVersion"></param>
-		/// <returns></returns>
+		/// <typeparam name="T">The common type which all events implement.</typeparam>
+		/// <param name="id">The value which uniquely identifies the aggregate of the events to be loaded.</param>
+		/// <param name="startingVersion">The version at which the loaded event stream should begin.</param>
+		/// <returns>A set of events for the aggregate indicated.</returns>
 		IEnumerable<T> LoadEvents<T>(Guid id, int startingVersion);
 
 		/// <summary>
@@ -24,23 +24,23 @@ namespace EventStore
 		/// <param name="id">The value which uniquely identifies the aggregate being persisted.</param>
 		/// <param name="aggregate">The type representing the aggregate being persisted.</param>
 		/// <param name="events">The events to be committed.</param>
-		/// <returns>Returns the version of the last event committed.</returns>
+		/// <returns>The version of the last event committed.</returns>
 		int StoreEvents(Guid id, Type aggregate, IEnumerable events);
 
 		/// <summary>
-		/// 
+		/// Loads the snapshot for the aggregate specified.
 		/// </summary>
-		/// <typeparam name="T"></typeparam>
-		/// <param name="id"></param>
-		/// <returns></returns>
+		/// <typeparam name="T">The type of snapshot being loaded.</typeparam>
+		/// <param name="id">The value which uniquely identifies the snapshot to be loaded.</param>
+		/// <returns>The snapshot for the identifier specified.</returns>
 		T LoadSnapshot<T>(Guid id);
 
 		/// <summary>
-		/// 
+		/// Saves the snapshot for a particular aggregate.
 		/// </summary>
-		/// <param name="id"></param>
-		/// <param name="version"></param>
-		/// <param name="snapshot"></param>
+		/// <param name="id">The value which uniquely identifies the aggregate to which the snapshot belongs.</param>
+		/// <param name="version">The version of the snapshot.</param>
+		/// <param name="snapshot">The snapshot momento to be saved.</param>
 		void StoreSnapshot(Guid id, int version, object snapshot);
 	}
 }
