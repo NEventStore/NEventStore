f168ac27f0d6fcaff38cda92536a749b1b902261
Oliver, Jonathan <jonathan.s.oliver42+git@gmail.com>
2010-12-11 23:35:53 -0700

    Overhauled SQL schema to reflect new model.

--- doc/MsSql.Schema.sql
+++ doc/MsSql.Schema.sql
@@ -1,59 +1,87 @@
-﻿/*
-DROP TABLE Events;
-DROP TABLE Commands;
-DROP TABLE Snapshots;
-DROP TABLE Aggregates;
-*/
-
-CREATE TABLE [dbo].[Aggregates]
+﻿CREATE TABLE [dbo].[Streams]
 (
-    [Id] [uniqueidentifier] NOT NULL,
-    [TenantId] [uniqueidentifier] NOT NULL,
-    [Version] [bigint] NOT NULL CHECK ([Version] > 0),
-    [Snapshot] [bigint] NOT NULL CHECK ([Snapshot] >= 0),
-    [Created] [datetime] NOT NULL DEFAULT (GETUTCDATE()),
-    [RuntimeType] [nvarchar](256) NOT NULL,
-    CONSTRAINT [PK_Aggregates] PRIMARY KEY CLUSTERED ([Id])
+       [StreamId] [uniqueidentifier] NOT NULL CHECK ([StreamId] != 0x0),
+       [Name] [nvarchar](256) NOT NULL,
+       [Revision] [bigint] NOT NULL CHECK ([Revision] > 0),
+       [Snapshot] [bigint] NOT NULL CHECK ([Snapshot] >= 0),
+       CONSTRAINT [PK_Streams] PRIMARY KEY CLUSTERED ([StreamId])
 )
 
-CREATE TABLE [dbo].[Commands]
+CREATE TABLE [dbo].[Commits]
 (
-    [Id] [uniqueidentifier] NOT NULL,
-    [Payload] [varbinary](MAX),
-    CONSTRAINT [PK_Commands] PRIMARY KEY CLUSTERED ([Id])
+       [StreamId] [uniqueidentifier] NOT NULL,
+       [Sequence] [bigint] NOT NULL CHECK ([Sequence] > 0),
+       [CommitId] [uniqueidentifier] NOT NULL,
+       [SystemSequence] [bigint] IDENTITY(1,1) NOT NULL,
+       [Revision] [bigint] NOT NULL CHECK ([Revision] > 0),
+       [Payload] [varbinary](MAX) NOT NULL CHECK (DATALENGTH([Payload]) > 0),
+       [Snapshot] [varbinary](MAX) NULL CHECK ([Snapshot] IS NULL OR DATALENGTH([Snapshot]) > 0),
+       CONSTRAINT [PK_Commits] PRIMARY KEY CLUSTERED ([StreamId], [Sequence])
+)
+CREATE UNIQUE NONCLUSTERED INDEX [IX_Commits] ON [dbo].[Commits] ([StreamId], [CommitId])
+
+CREATE TABLE [dbo].[Dispatch]
+(
+       [DispatchId] [bigint] IDENTITY(1,1) NOT NULL,
+       [StreamId] [uniqueidentifier] NOT NULL,
+       [Sequence] [bigint] NOT NULL,
+       CONSTRAINT [PK_Dispatch] PRIMARY KEY CLUSTERED ([DispatchId])
 )
 
-CREATE TABLE [dbo].[Events]
-(
-    [Id] [uniqueidentifier] NOT NULL,
-    [Version] [bigint] NOT NULL CHECK ([Version] > 0),
-    [CommitSequence] [bigint] IDENTITY(1,1) NOT NULL,
-    [Created] [datetime] NOT NULL DEFAULT (GETUTCDATE()),
-    [CommandId] [uniqueidentifier],
-    [Payload] [varbinary](MAX) NOT NULL,
-    CONSTRAINT [PK_Events] PRIMARY KEY CLUSTERED ([Id], [Version])
-)
+ALTER TABLE [dbo].[Commits]  WITH CHECK ADD CONSTRAINT [FK_Commits_Streams] FOREIGN KEY([StreamId])
+REFERENCES [dbo].[Streams] ([StreamId])
+ALTER TABLE [dbo].[Commits] CHECK CONSTRAINT [FK_Commits_Streams]
 
-CREATE TABLE [dbo].[Snapshots]
-(
-    [Id] [uniqueidentifier] NOT NULL,
-    [Version] [bigint] NOT NULL CHECK ([Version] > 0),
-    [Created] [datetime] NOT NULL DEFAULT (GETUTCDATE()),
-    [Payload] [varbinary](MAX) NOT NULL,
-    CONSTRAINT [PK_Snapshots] PRIMARY KEY CLUSTERED ([Id], [Version])
-)
+ALTER TABLE [dbo].[Dispatch]  WITH CHECK ADD CONSTRAINT [FK_Dispatch_Commits] FOREIGN KEY([StreamId], [Sequence])
+REFERENCES [dbo].[Commits] ([StreamId], [Sequence])
+ALTER TABLE [dbo].[Dispatch] CHECK CONSTRAINT [FK_Dispatch_Commits]
 
-CREATE UNIQUE NONCLUSTERED INDEX [IX_Events] ON [dbo].[Events] ([CommitSequence])
-CREATE INDEX [IX_Events_CommandId] ON [Events] ([CommandId])
+GO
+CREATE TRIGGER [dbo].[AddDispatch] ON [dbo].[Commits] AFTER INSERT
+AS BEGIN
 
-ALTER TABLE [dbo].[Events] WITH CHECK ADD CONSTRAINT [FK_Events_Aggregates] FOREIGN KEY([Id])
-REFERENCES [dbo].[Aggregates] ([Id])
-ALTER TABLE [dbo].[Events] CHECK CONSTRAINT [FK_Events_Aggregates]
+       SET NOCOUNT ON;
 
-ALTER TABLE [dbo].[Snapshots] WITH CHECK ADD CONSTRAINT [FK_Snapshots_Aggregates] FOREIGN KEY([Id])
-REFERENCES [dbo].[Aggregates] ([Id])
-ALTER TABLE [dbo].[Snapshots] CHECK CONSTRAINT [FK_Snapshots_Aggregates]
+       INSERT
+         INTO [dbo].[Dispatch]
+            ( [StreamId], [Sequence] )
+       SELECT [StreamId], [Sequence]
+         FROM [Inserted];
 
-ALTER TABLE [dbo].[Events] WITH CHECK ADD CONSTRAINT [FK_Events_Commands] FOREIGN KEY([CommandId])
-REFERENCES [dbo].[Commands] ([Id])
-ALTER TABLE [dbo].[Events] CHECK CONSTRAINT [FK_Events_Aggregates]
+END;
+
+GO
+CREATE TRIGGER [dbo].[IncrementStreamRevision] ON [dbo].[Commits] AFTER INSERT
+AS BEGIN
+
+       SET NOCOUNT ON;
+       
+       UPDATE [dbo].[Streams]
+          SET [Revision] = [I].[Revision]
+         FROM [dbo].[Streams] AS [S]
+        INNER JOIN [Inserted] AS [I]
+           ON [S].[StreamId] = [I].[StreamId]
+        WHERE [S].[Revision] < [I].[Revision];
+
+END;
+
+GO
+ALTER TRIGGER [dbo].[SetStreamSnapshotRevision] ON [dbo].[Commits] AFTER UPDATE
+AS BEGIN
+
+       IF NOT UPDATE([Snapshot]) RETURN;
+       SET NOCOUNT ON;
+
+       UPDATE [dbo].[Streams]
+          SET [Snapshot] = [U].[Revision]
+         FROM [dbo].[Streams] AS [S]
+        INNER JOIN [Updated] AS [U]
+           ON [S].[StreamId] = [U].[StreamId]
+        WHERE [U].[Snapshot] IS NOT NULL
+          AND [S].[Snapshot] < [U].[Revision];
+          
+       -- TODO: if the snapshot is null, set the stream snapshot to be the most recent snapshot
+          
+END;
+
+-- TODO: if any other column in the Commits table was altered, raise an error
