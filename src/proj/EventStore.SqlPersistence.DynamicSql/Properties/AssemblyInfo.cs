72213f2c0950e2e923018bce94320666e6ffbab1
Oliver, Jonathan <joliver@localhost>
2010-12-14 14:28:43 -0700

    Removed unused projects.

--- src/proj/EventStore.SqlPersistence.DynamicSql/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("EventStore.SqlPersistence.DynamicSql")]
-[assembly: AssemblyDescription("")]
-[assembly: Guid("952c8529-9c07-4305-b460-6a5bf0f6ae51")]
--- src/tests/EventStore.Core.IntegrationTests/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("EventStore.Core.IntegrationTests")]
-[assembly: AssemblyDescription("")]
-[assembly: Guid("27614454-1bae-4497-9603-1a774813c573")]
