80dd58febd20be45b5ca59745c749fda5e409a1d
Oliver, Jonathan <jonathan.s.oliver42+git@gmail.com>
2010-12-14 20:24:50 -0700

    Added XmlSerializer.

---
+++ src/proj/EventStore.Serialization/XmlSerializer.cs
@@ -0,0 +1,20 @@
+namespace EventStore.Serialization
+{
+	using System.IO;
+	using System.Runtime.Serialization;
+
+	public class XmlSerializer : ISerialize
+	{
+		private readonly DataContractSerializer serializer = new DataContractSerializer(typeof(Commit));
+
+		public virtual void Serialize(Stream output, object graph)
+		{
+			if (null != graph)
+				this.serializer.WriteObject(output, graph);
+		}
+		public virtual object Deserialize(Stream input)
+		{
+			return this.serializer.ReadObject(input);
+		}
+	}
+}
