9fc043b4881e9304fce0e47526034454090a386e
Oliver, Jonathan <jonathan.s.oliver42+git@gmail.com>
2010-12-30 19:28:48 -0700

    Overhauled acceptance test automation script.

--- acceptance.cmd
+++ acceptance.cmd
@@ -1,41 +1,30 @@
 @ECHO OFF
+SETLOCAL
 
-ECHO SQL Server
-SET persistence=MsSqlPersistence
-SET host=localhost
-SET database=EventStore2
-"bin/machine.specifications-bin/.NET 4.0/mspec.exe" src/tests/EventStore.Persistence.AcceptanceTests/bin/Debug/EventStore.Persistence.AcceptanceTests.dll
+CALL :run_test MsSqlPersistence localhost 0 EventStore2 "" ""
+REM CALL :run_test SqlitePersistence localhost 0 EventStore2 "" ""
+CALL :run_test SqlCePersistence localhost 0 EventStore2 "" ""
+CALL :run_test AccessPersistence localhost 0 EventStore2 "" ""
+CALL :run_test MySqlPersistence localhost 0 EventStore2 root ""
+CALL :run_test PostgreSqlPersistence localhost 0 EventStore2 postgres ""
+CALL :run_test FirebirdPersistence localhost 0 /var/lib/firebird/data/EventStore2.fdb SYSDBA masterkey
 
-ECHO SQL Server Compact
-SET persistence=SqlCePersistence
-SET host=localhost
-SET database=EventStore2
-"bin/machine.specifications-bin/.NET 4.0/mspec.exe" src/tests/EventStore.Persistence.AcceptanceTests/bin/Debug/EventStore.Persistence.AcceptanceTests.dll
+ENDLOCAL
+GOTO :eof 
 
-ECHO Access
-SET persistence=AccessPersistence
-SET host=localhost
-SET database=EventStore2
-"bin/machine.specifications-bin/.NET 4.0/mspec.exe" src/tests/EventStore.Persistence.AcceptanceTests/bin/Debug/EventStore.Persistence.AcceptanceTests.dll
+:run_test <persistence> <host> <port> <database> <user> <password>
+SETLOCAL
 
-ECHO MySQL
-SET persistence=MySqlPersistence
-SET host=localhost
-SET user=root
-SET database=EventStore2
-"bin/machine.specifications-bin/.NET 4.0/mspec.exe" src/tests/EventStore.Persistence.AcceptanceTests/bin/Debug/EventStore.Persistence.AcceptanceTests.dll
+SET persistence=%~1
+SET host=%~2
+SET port=%~3
+SET database=%~4
+SET user=%~5
+SET password=%~6
 
-ECHO PostgreSQL
-SET persistence=PostgreSqlPersistence
-SET host=localhost
-SET user=postgres
-SET database=EventStore2
+ECHO ===============
+ECHO TESTING: %~1
 "bin/machine.specifications-bin/.NET 4.0/mspec.exe" src/tests/EventStore.Persistence.AcceptanceTests/bin/Debug/EventStore.Persistence.AcceptanceTests.dll
+ECHO.
 
-ECHO Firebird
-SET persistence=FirebirdPersistence
-SET host=localhost
-SET user=SYSDBA
-SET password=masterkey
-SET database="/var/lib/firebird/data/EventStore2.fdb"
-"bin/machine.specifications-bin/.NET 4.0/mspec.exe" src/tests/EventStore.Persistence.AcceptanceTests/bin/Debug/EventStore.Persistence.AcceptanceTests.dll
+ENDLOCAL
