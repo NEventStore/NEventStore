2c1fac0920c33e509071b0b4d060279eb8cc92cb
Oliver, Jonathan <joliver@localhost>
2011-01-05 11:30:47 -0700

    Made *massive* BREAKING CHANGE to ProtocolBufferSerializer--now using 16-byte key instead of 4-byte key to identify types.
    According to the MSDN docs, GetHashCode() on a string is subject to change between processor architectures and versions of the .NET runtime.  This means that it's not suitable value to be used for longterm persistence such as with serialization to a disk.  We are now using MD5 serialization of a string to get a 16-byte key (which we treat as a GUID for simplicity).  This 16-byte key allows us to unambiguously identify message serialized to the disk but at a slightly higher cost in terms of storage space (16 bytes vs 4 bytes).

--- src/proj/EventStore.Serialization/ProtocolBufferSerializer.cs
+++ src/proj/EventStore.Serialization/ProtocolBufferSerializer.cs
@@ -6,12 +6,17 @@ namespace EventStore.Serialization
 	using System.Linq;
 	using System.Reflection;
 	using System.Runtime.Serialization;
+	using System.Security.Cryptography;
+	using System.Text;
+	using Persistence;
 	using ProtoBuf;
 
 	public class ProtocolBufferSerializer : ISerialize
 	{
-		private readonly Dictionary<int, Type> hashes = new Dictionary<int, Type>();
-		private readonly Dictionary<Type, int> types = new Dictionary<Type, int>();
+		private const int GuidLengthInBytes = 16;
+		private static readonly MD5 TypeHasher = MD5.Create(); // creates 16-byte hashes
+		private readonly Dictionary<Guid, Type> hashes = new Dictionary<Guid, Type>();
+		private readonly Dictionary<Type, Guid> types = new Dictionary<Type, Guid>();
 		private readonly Dictionary<Type, Func<Stream, object>> deserializers =
 			new Dictionary<Type, Func<Stream, object>>();
 
@@ -20,34 +25,82 @@ namespace EventStore.Serialization
 		{
 		}
 		public ProtocolBufferSerializer(params Assembly[] contractAssemblies)
+			: this(contractAssemblies.SelectMany(assembly => assembly.GetTypes()).ToArray())
 		{
-			foreach (var type in contractAssemblies.SelectMany(assembly => assembly.GetTypes()))
-				this.RegisterContract(type);
 		}
 		public ProtocolBufferSerializer(params Type[] dataContracts)
+			: this()
 		{
 			foreach (var contract in dataContracts ?? new Type[] { })
 				this.RegisterContract(contract);
 		}
+		public ProtocolBufferSerializer()
+		{
+			this.RegisterPrimitives();
+			this.RegisterCommonTypes();
+		}
+
+		private void RegisterPrimitives()
+		{
+			this.RegisterContract(typeof(bool));
+
+			this.RegisterContract(typeof(char));
+			this.RegisterContract(typeof(byte));
+			this.RegisterContract(typeof(sbyte));
+
+			this.RegisterContract(typeof(short));
+			this.RegisterContract(typeof(ushort));
+			this.RegisterContract(typeof(int));
+			this.RegisterContract(typeof(uint));
+			this.RegisterContract(typeof(long));
+			this.RegisterContract(typeof(ulong));
+			this.RegisterContract(typeof(double));
+			this.RegisterContract(typeof(float));
+			this.RegisterContract(typeof(decimal));
+
+			this.RegisterContract(typeof(string));
+		}
+		private void RegisterCommonTypes()
+		{
+			this.RegisterContract(typeof(Commit));
+			this.RegisterContract(typeof(EventMessage));
+			this.RegisterContract(typeof(Dictionary<string, object>));
+			this.RegisterContract(typeof(List<object>));
+			this.RegisterContract(typeof(List<EventMessage>));
+			this.RegisterContract(typeof(LinkedList<object>));
+			this.RegisterContract(typeof(LinkedList<EventMessage>));
+
+			this.RegisterContract(typeof(Uri));
+			this.RegisterContract(typeof(Guid));
+			this.RegisterContract(typeof(Exception));
+			this.RegisterContract(typeof(SerializationException));
+		}
 		private void RegisterContract(Type contract)
 		{
-			if (!this.CanRegisterContract(contract) || string.IsNullOrEmpty(contract.FullName))
+			if (!this.CanRegisterContract(contract))
 				return;
 
-			// TODO: GetHashCode() is a *terrible* way to get contract "identity".
-			var hash = contract.FullName.GetHashCode();
-			this.hashes[hash] = contract;
-			this.types[contract] = hash;
-
-			// TODO: make this faster by using reflection to create a delegate and then invoking the delegate
-			var deserialize = typeof(Serializer).GetMethod("Deserialize").MakeGenericMethod(contract);
-			this.deserializers[contract] = stream => deserialize.Invoke(null, new object[] { stream });
+			this.RegisterHash(contract);
+			this.RegisterDeserializer(contract);
 		}
 		private bool CanRegisterContract(Type contract)
 		{
 			return contract != null
 				&& !this.types.ContainsKey(contract)
-				&& contract.HasAttribute<DataContractAttribute>();
+				&& !string.IsNullOrEmpty(contract.FullName);
+		}
+		private void RegisterHash(Type contract)
+		{
+			var bytes = Encoding.Unicode.GetBytes(contract.FullName ?? string.Empty);
+			var hash = new Guid(TypeHasher.ComputeHash(bytes));
+			this.hashes[hash] = contract;
+			this.types[contract] = hash;
+		}
+		private void RegisterDeserializer(Type contract)
+		{
+			// TODO: make this faster by using reflection to create a delegate and then invoking the delegate
+			var deserialize = typeof(Serializer).GetMethod("Deserialize").MakeGenericMethod(contract);
+			this.deserializers[contract] = stream => deserialize.Invoke(null, new object[] { stream });
 		}
 
 		public virtual void Serialize(Stream output, object graph)
@@ -61,12 +114,12 @@ namespace EventStore.Serialization
 		}
 		private void WriteContractTypeToStream(Stream output, Type contract)
 		{
-			int hash;
+			Guid hash;
 			if (!this.types.TryGetValue(contract, out hash))
 				throw new SerializationException(ExceptionMessages.UnableToSerialize.FormatWith(contract));
 
-			var header = BitConverter.GetBytes(hash);
-			output.Write(header, 0, header.Length);
+			var header = hash.ToByteArray();
+			output.Write(header, 0, GuidLengthInBytes);
 		}
 
 		public virtual object Deserialize(Stream input)
@@ -79,9 +132,9 @@ namespace EventStore.Serialization
 		}
 		private Type ReadContractType(Stream serialized)
 		{
-			var header = new byte[4];
+			var header = new byte[GuidLengthInBytes];
 			serialized.Read(header, 0, header.Length);
-			var hash = BitConverter.ToInt32(header, 0);
+			var hash = new Guid(header);
 
 			Type contract;
 			if (!this.hashes.TryGetValue(hash, out contract))
