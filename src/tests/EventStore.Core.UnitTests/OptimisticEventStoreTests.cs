e68c9f31c746f6fafd152164d0ad513b5b11c36e
anavarro9731 <anavarro9731@gmail.com>
2011-01-11 19:48:34 +1000

    Added Test to show concurrency problem in OptimisticEventStore

--- src/tests/EventStore.Core.UnitTests/OptimisticEventStoreTests.cs
+++ src/tests/EventStore.Core.UnitTests/OptimisticEventStoreTests.cs
@@ -10,6 +10,7 @@ namespace EventStore.Core.UnitTests
 	using Moq;
 	using Persistence;
 	using It = Machine.Specifications.It;
+    using System.Threading;
 
 	[Subject("OptimisticEventStore")]
 	public class when_reading_a_stream_up_to_a_maximum_revision : using_persistence
@@ -541,6 +542,96 @@ namespace EventStore.Core.UnitTests
 		It should_throw_a_ConcurrencyException = () =>
 			thrown.ShouldBeOfType<ConcurrencyException>();
 	}
+
+    [Subject("OptimisticEventstore")]
+    public class when_one_thread_persists_a_commit_whose_commitsequence_or_revision_was_made_stale_by_another_thread :
+        using_persistence
+    {
+        const int MostRecentStreamRevision = 1;
+        const int CommitSequence = 1;
+
+        static readonly Commit[] Commits = new[]
+            {
+                new Commit(streamId, MostRecentStreamRevision, Guid.NewGuid(), CommitSequence, null, null, null),
+            };
+
+        static readonly CountdownEvent countdown = new CountdownEvent(2);
+        static readonly AutoResetEvent gate = new AutoResetEvent(false);
+        static int ReadCount, WriteCount;
+        static readonly object readLock = new object();
+
+        static Exception thrown;
+
+        Establish context = () =>
+        {
+            persistence.Setup(x => x.GetUntil(streamId, int.MaxValue)).Returns(Commits)
+                .Callback(() =>
+                {
+                    bool wait = false;
+                    lock (readLock)
+                    {
+                        ReadCount++;
+                        if (ReadCount == 1) wait = true; //whoever gets here first wait
+                    }
+                    if (wait)
+                    {
+                        gate.WaitOne();
+                    }
+                });
+
+            dispatcher.Setup(x => x.Dispatch(Moq.It.IsAny<Commit>()))
+                .Callback(() =>
+                {
+                    WriteCount++;
+                    //release the other thread once we have persisted the first commit
+                    if (WriteCount == 1) gate.Set();
+                    countdown.Signal(1);
+                });
+        };
+
+        static void ConcurrencyTest()
+        {
+            //both threads share a consistent view of the data after reading
+            //as such they submit conflicting commits
+            try
+            {
+                store.ReadUntil(streamId, 0);
+
+                var attempt = new CommitAttempt() //create inline to avoid DuplicateCommitException
+                {
+                    StreamId = streamId,
+                    CommitId = Guid.NewGuid(),
+                    PreviousCommitSequence = CommitSequence,
+                    StreamRevision = MostRecentStreamRevision + 1,
+                    Events = { new EventMessage() }
+                };
+                store.Write(attempt);
+            }
+            catch (Exception e)
+            {
+                thrown = e;
+                countdown.Signal(1);
+            }
+        }
+
+        Because of = () =>
+        {
+            var threadOne = new Thread(ConcurrencyTest);
+            var threadTwo = new Thread(ConcurrencyTest);
+
+            threadOne.Start();
+            threadTwo.Start();
+            countdown.Wait();
+        };
+
+        It should_throw_a_ConcurrencyException = () => thrown.ShouldBeOfType<ConcurrencyException>();
+
+        Cleanup thisFunction = () =>
+        {
+            gate.Dispose();
+            countdown.Dispose();
+        };
+    }
 
 	public abstract class using_persistence
 	{
