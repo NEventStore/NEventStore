89a28440051595907195d612cf5d4496a1f9dfad
Oliver, Jonathan <jonathan.s.oliver42+git@gmail.com>
2011-02-03 08:13:26 -0700

    MongoPersistence now uses DateTime.UtcNow.

--- src/proj/EventStore.Persistence.MongoPersistence/ExtensionMethods.cs
+++ src/proj/EventStore.Persistence.MongoPersistence/ExtensionMethods.cs
@@ -19,7 +19,7 @@
 				StartingStreamRevision = commit.StreamRevision - (commit.Events.Count - 1),
 				StreamRevision = commit.StreamRevision,
 				CommitId = commit.CommitId,
-				CommitStamp = DateTime.Now,
+				CommitStamp = DateTime.UtcNow,
 				Headers = commit.Headers,
 				Payload = payload
 			};
