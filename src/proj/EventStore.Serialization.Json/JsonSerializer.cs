5bad58c564b188724b13b5c8c815c253124a2405
Oliver, Jonathan <jonathan.s.oliver42+git@gmail.com>
2011-02-10 05:48:13 -0700

    Explicitly specifying text encoding for JSON serializer.

--- src/proj/EventStore.Serialization.Json/JsonSerializer.cs
+++ src/proj/EventStore.Serialization.Json/JsonSerializer.cs
@@ -1,6 +1,7 @@
 namespace EventStore.Serialization
 {
 	using System.IO;
+	using System.Text;
 	using Newtonsoft.Json;
 
 	public class JsonSerializer : ISerialize
@@ -14,13 +15,13 @@ namespace EventStore.Serialization
 
 		public void Serialize(Stream output, object graph)
 		{
-			using (var streamWriter = new StreamWriter(output))
+			using (var streamWriter = new StreamWriter(output, Encoding.UTF8))
 			using (var writer = new JsonTextWriter(streamWriter))
 				this.serializer.Serialize(writer, graph);
 		}
 		public object Deserialize(Stream input)
 		{
-			using (var streamReader = new StreamReader(input))
+			using (var streamReader = new StreamReader(input, Encoding.UTF8))
 			using (var reader = new JsonTextReader(streamReader))
 				return this.serializer.Deserialize(reader);
 		}
