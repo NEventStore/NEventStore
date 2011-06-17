075a13fb38783ec215f2bd653180c4f6e26fe2cb
CaptainCodeman <simon@captaincodeman.com>
2011-06-16 22:40:50 -0600

    Command file to install package dependencies

---
+++ install-packages.cmd
@@ -0,0 +1,7 @@
+nuget\nuget.exe i src\proj\EventStore.Persistence.MongoPersistence\packages.config -o Packages
+nuget\nuget.exe i src\proj\EventStore.Persistence.RavenPersistence\packages.config -o Packages
+nuget\nuget.exe i src\proj\EventStore.Serialization.Json\packages.config -o Packages
+nuget\nuget.exe i src\proj\EventStore.Serialization.ServiceStack\packages.config -o Packages
+nuget\nuget.exe i src\tests\EventStore.Core.UnitTests\packages.config -o Packages
+nuget\nuget.exe i src\tests\EventStore.Persistence.AcceptanceTests\packages.config -o Packages
+nuget\nuget.exe i src\tests\EventStore.Serialization.AcceptanceTests\packages.config -o Packages
