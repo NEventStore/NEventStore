7f1f2bd9de44475b54685b270f2b4de888432c45
John Downey <jdowney@gmail.com>
2011-07-28 08:53:33 -0400

    Fixed minor spelling mistake

--- doc/EventStore.Example/MainProgram.cs
+++ doc/EventStore.Example/MainProgram.cs
@@ -5,7 +5,7 @@ namespace EventStore.Example
 
 	internal static class MainProgram
 	{
-		private static readonly Guid StreamId = Guid.NewGuid(); // aggregate identifer
+		private static readonly Guid StreamId = Guid.NewGuid(); // aggregate identifier
 		private static readonly byte[] EncryptionKey = new byte[]
 		{
 			0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf
