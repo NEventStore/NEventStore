08949810bba1768e0a520729fab42e757d7dd98a
Oliver, Jonathan <jonathan.s.oliver42+git@gmail.com>
2011-08-31 21:13:21 -0600

    Adding paging behavior to SQL Server using CTEs, which requires SQL 2005 or later.

--- src/tests/EventStore.Persistence.AcceptanceTests/PersistenceTests.cs
+++ src/tests/EventStore.Persistence.AcceptanceTests/PersistenceTests.cs
@@ -185,6 +185,7 @@ namespace EventStore.Persistence.AcceptanceTests
 	public class when_committing_more_events_than_the_configured_page_size : using_the_persistence_engine
 	{
 		static readonly int ConfiguredPageSize = FactoryScanner.PageSize;
+		static readonly int CommitsToPersist = ConfiguredPageSize + 1;
 		static readonly HashSet<Guid> committed = new HashSet<Guid>();
 		static readonly ICollection<Guid> loaded = new LinkedList<Guid>();
 		static Commit attempt = streamId.BuildAttempt();
@@ -192,7 +193,7 @@ namespace EventStore.Persistence.AcceptanceTests
 		Establish context = () =>
 		{
 			var attempt = streamId.BuildAttempt();
-			for (var i = 0; i < ConfiguredPageSize + 1; i++)
+			for (var i = 0; i < CommitsToPersist; i++)
 			{
 				persistence.Commit(attempt);
 				committed.Add(attempt.CommitId);
@@ -204,10 +205,10 @@ namespace EventStore.Persistence.AcceptanceTests
 			persistence.GetFrom(streamId, 0, int.MaxValue).ToList().ForEach(x => loaded.Add(x.CommitId));
 
 		It should_load_the_same_number_of_commits_which_have_been_persisted = () =>
-			committed.Count.ShouldEqual(loaded.Count);
+			loaded.Count.ShouldEqual(committed.Count);
 
 		It should_load_the_same_commits_which_have_been_persisted = () =>
-			committed.All(x => loaded.Contains(x)).ShouldBeTrue();
+			committed.All(x => loaded.Contains(x)).ShouldBeTrue(); // all commits should be found in loaded collection
 	}
 
 	[Subject("Persistence")]
