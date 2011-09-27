33806b10fbe6b9ce0b85cdd789306fb8861cfd27
Mikael Ostberg <me@mikeeast.it>
2011-09-27 03:16:18 +0200

    Implemented an event converting IPipelineHook

---
+++ src/proj/EventStore/IConvertEvents.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EventStore
+{
+    interface IConvertEvents<in TSource, out TTarget>
+    {
+        TTarget Convert(TSource sourceEvent);
+    }
+}
