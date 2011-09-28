1d86eb0a38229061fb77f85d237f5d6c4541bdb2
Mikael Ostberg <me@mikeeast.it>
2011-09-29 01:37:47 +0200

    Changed the implementation from being a IPipelineHook to being its own concept

---
+++ src/proj/EventStore.Core/EventUpconverterEngine.cs
@@ -0,0 +1,57 @@
+﻿namespace EventStore
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using Logging;
+
+    public class EventUpconverterEngine : IConvertCommits
+    {
+        private static readonly ILog Logger = LogFactory.BuildLogger(typeof(EventUpconverterEngine));
+        private readonly Dictionary<Type, Func<object, object>> converters;
+
+        public EventUpconverterEngine(IEnumerable<Assembly> assemblies)
+        {
+            var c = from a in assemblies
+                    from t in a.GetTypes()
+                    let i = t.GetInterface(typeof(IConvertEvents<,>).FullName)
+                    where i != null
+                    let sourceType = i.GetGenericArguments().First()
+                    let convertMethod = i.GetMethod("Convert", BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic )
+                    let instance = Activator.CreateInstance(t)
+                    select new KeyValuePair<Type, Func<object, object>>(
+                        sourceType,
+                        body => convertMethod.Invoke(instance, new[] { body as object })
+                    );
+            this.converters = c.ToDictionary(x => x.Key, x => x.Value);
+        }
+
+        private object Convert(object body)
+        {
+            Func<object, object> converter;
+            object result = body;
+            if (this.converters.TryGetValue(body.GetType(), out converter))
+            {
+                result = Convert(converter(body));
+                Logger.Debug(Resources.ConvertingEvent, body.GetType(), result.GetType());
+            }
+            return result;
+        }
+
+        public Commit Convert(Commit committed)
+        {
+            foreach (var eventMessage in committed.Events)
+            {
+                eventMessage.Body = Convert(eventMessage.Body);
+            }
+            return committed;
+        }
+
+        public void Dispose()
+        {
+            this.converters.Clear();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ src/proj/EventStore.Wireup/EventUpconverterWireup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace EventStore
+{
+    using System.Reflection;
+    using Logging;
+
+    public class EventUpconverterWireup : Wireup
+    {
+        private static readonly ILog Logger = LogFactory.BuildLogger(typeof(EventUpconverterWireup));
+        private readonly List<Assembly> assembliesToScan = new List<Assembly>();
+
+        public EventUpconverterWireup(Wireup wireup) : base(wireup)
+        {
+            Logger.Debug(Messages.EventUpconverterRegistered);
+
+            this.Container.Register<IConvertCommits>(c =>
+            {
+                if (!assembliesToScan.Any())
+                    assembliesToScan.AddRange(getAllAssemblies());
+                return new EventUpconverterEngine(assembliesToScan);
+            });
+        }
+
+        private IEnumerable<Assembly> getAllAssemblies()
+        {
+            return Assembly.GetCallingAssembly()
+                .GetReferencedAssemblies()
+                .Select(Assembly.Load)
+                .Concat(new[] {Assembly.GetCallingAssembly()});
+        }
+
+        public virtual EventUpconverterWireup UsingConvertersFrom(params Assembly[] assemblies)
+        {
+            Logger.Debug(Messages.EventUpconvertersLoadedFrom, string.Concat(", ", assemblies));
+            this.assembliesToScan.AddRange(assemblies);
+            return this;
+        }
+
+        public virtual EventUpconverterWireup UsingConvertersFromAssemblyContaining(params Type[] converters)
+        {
+            var assemblies = converters.Select(c => c.Assembly)
+                .Distinct();
+            Logger.Debug(Messages.EventUpconvertersLoadedFrom, string.Concat(", ", assemblies));
+            this.assembliesToScan.AddRange(assemblies);
+            return this;
+        }
+    }
+}
