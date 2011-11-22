38f362d8b931870e4a27232fcda468cf2e050a1f
Oliver, Jonathan <jonathan.s.oliver42+git@gmail.com>
2011-11-21 20:13:37 -0700

    GetUndispatchedCommits now returns an array instead of a paged result set, closes issue #89.

--- src/proj/EventStore.Persistence.SqlPersistence/SqlPersistenceEngine.cs
+++ src/proj/EventStore.Persistence.SqlPersistence/SqlPersistenceEngine.cs
@@ -158,8 +158,9 @@ namespace EventStore.Persistence.SqlPersistence
 		{
 			Logger.Debug(Messages.GettingUndispatchedCommits);
 			return this.ExecuteQuery(Guid.Empty, query =>
-				query.ExecutePagedQuery(this.dialect.GetUndispatchedCommits, (q, r) => { }))
-					.Select(x => x.GetCommit(this.serializer));
+				query.ExecuteWithQuery(this.dialect.GetUndispatchedCommits))
+					.Select(x => x.GetCommit(this.serializer))
+					.ToArray();
 		}
 		public virtual void MarkCommitAsDispatched(Commit commit)
 		{
