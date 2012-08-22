d1b051dae54772138579340e07b7870bf27d7edb
Jonathan Matheus <jmatheus@gmail.com>
2012-08-22 16:15:48 -0500

    Added partition in the raven wireup api

--- src/proj/EventStore.Persistence.RavenPersistence.Wireup/RavenPersistenceWireup.cs
+++ src/proj/EventStore.Persistence.RavenPersistence.Wireup/RavenPersistenceWireup.cs
@@ -17,8 +17,9 @@ namespace EventStore
 		private IDocumentSerializer serializer = new DocumentObjectSerializer();
 		private Uri url;
 		private string defaultDatabase;
+	    private string partition = null;
 
-		public RavenPersistenceWireup(Wireup inner)
+	    public RavenPersistenceWireup(Wireup inner)
 			: this(inner, string.Empty)
 		{
 		}
@@ -37,7 +38,8 @@ namespace EventStore
 				RequestedPageSize = this.pageSize,
 				ConnectionName = connectionName,
 				DefaultDatabase = this.defaultDatabase,
-				Url = this.url
+				Url = this.url,
+                Partition = this.partition
 			});
 
 			this.Container.Register(c => new RavenPersistenceFactory(c.Resolve<RavenConfiguration>()).Build());
@@ -50,6 +52,7 @@ namespace EventStore
 			this.defaultDatabase = database;
 			return this;
 		}
+
 		public virtual RavenPersistenceWireup Url(string address)
 		{
 			Logger.Debug("Using database at '{0}'.", address);
@@ -58,6 +61,13 @@ namespace EventStore
 			return this;
 		}
 
+        public virtual RavenPersistenceWireup Partition(string name)
+        {
+            this.partition = name;
+
+            return this;
+        }
+
 		public virtual RavenPersistenceWireup PageEvery(int records)
 		{
 			Logger.Debug("Page result set every {0} records.", records);
@@ -65,6 +75,7 @@ namespace EventStore
 			this.pageSize = records;
 			return this;
 		}
+
 		public virtual RavenPersistenceWireup MaxServerPageSizeConfiguration(int records)
 		{
 			Logger.Debug("The maximum allowed page size as configured on the Raven server is {0} records.", records);
@@ -80,10 +91,12 @@ namespace EventStore
 			this.consistentQueries = fullyConsistent;
 			return this;
 		}
+
 		public virtual RavenPersistenceWireup ConsistentQueries()
 		{
 			return this.ConsistentQueries(true);
 		}
+
 		public virtual RavenPersistenceWireup StaleQueries()
 		{
 			return this.ConsistentQueries(false);
@@ -96,6 +109,7 @@ namespace EventStore
 			this.serializer = instance;
 			return this;
 		}
+        
 		private IDocumentSerializer ResolveSerializer(NanoContainer container)
 		{
 			var registered = container.Resolve<ISerialize>();
