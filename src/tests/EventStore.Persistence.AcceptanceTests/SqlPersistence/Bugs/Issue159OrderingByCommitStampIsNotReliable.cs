7bfdb5db1a5242bf39efc4acd41fb16ed41f4d32
Damian Hickey <dhickey@gmail.com>
2013-05-16 23:17:33 +0100

    Failing test for Issue #159 - commit order not reliable when getting undispatched events

---
+++ src/tests/EventStore.Persistence.AcceptanceTests/SqlPersistence/Bugs/Issue159OrderingByCommitStampIsNotReliable.cs
@@ -0,0 +1,32 @@
+﻿namespace EventStore.Persistence.AcceptanceTests.SqlPersistence.Bugs
+{
+    using System;
+    using System.Linq;
+    using Machine.Specifications;
+
+    public class Issue159OrderingByCommitStampIsNotReliable : using_the_persistence_engine
+    {
+        static Commit[] undispatched;
+
+        private Establish context = () =>
+                                    {
+                                        persistence.Purge();
+                                        var dateTime = new DateTime(2013, 1, 1);
+                                        SystemTime.Resolver = () => dateTime;
+                                        // deliberately out of order
+                                        persistence.Commit(new Commit(streamId, 1, Guid.NewGuid(), 1, SystemTime.UtcNow, null, null));
+                                        persistence.Commit(new Commit(streamId, 3, Guid.NewGuid(), 3, SystemTime.UtcNow, null, null)); 
+                                        persistence.Commit(new Commit(streamId, 2, Guid.NewGuid(), 2, SystemTime.UtcNow, null, null));
+                                    };
+
+        private Because of = () => undispatched = persistence.GetUndispatchedCommits().ToArray();
+
+        private It should_have_commits_in_correct_order = () =>
+                                                          {
+                                                              for (var i = 1; i <= undispatched.Length; i++)
+                                                              {
+                                                                  undispatched[i].CommitSequence.ShouldEqual(i);
+                                                              }
+                                                          };
+    }
+}
