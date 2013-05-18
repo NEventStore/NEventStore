ec61a65b3b047ab0fea2141741e29e877740b404
Damian Hickey <dhickey@gmail.com>
2013-05-18 08:39:27 +0100

    Remove NLog and Log4Net projects, nuspecs and build tasks.

--- src/proj/EventStore.Logging.Log4Net/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("EventStore.Logging.Log4Net")]
-[assembly: AssemblyDescription("")]
-[assembly: Guid("68f81b23-8823-43f7-90bd-f68884a1bedb")]
--- src/proj/EventStore.Logging.NLog/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("EventStore.Logging.NLog")]
-[assembly: AssemblyDescription("")]
-[assembly: Guid("4e33b458-8aeb-4ec6-8f74-2ead32e11a6d")]
