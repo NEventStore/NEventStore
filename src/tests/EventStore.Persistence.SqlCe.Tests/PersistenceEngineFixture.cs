9c0e8a77bc5e869345a3d8c8f102ea2c4c970a26
Damian Hickey <dhickey@gmail.com>
2013-06-03 23:32:59 +0100

    PersistenceEngineFixture for SqlCe (incomplete)

---
+++ src/tests/EventStore.Persistence.SqlCe.Tests/PersistenceEngineFixture.cs
@@ -0,0 +1,21 @@
+﻿namespace EventStore.Persistence.AcceptanceTests
+{
+    using System.Transactions;
+    using Serialization;
+    using SqlPersistence;
+    using SqlPersistence.SqlDialects;
+
+    public partial class PersistenceEngineFixture
+    {
+        public PersistenceEngineFixture()
+        {
+            this.CreatePersistence = () => 
+                new SqlPersistenceFactory(
+                    new ConfigurationConnectionFactory("name"),
+                    new BinarySerializer(),
+                    new SqlCeDialect(),
+                    TransactionScopeOption.Suppress,
+                    10).Build();
+        }
+    }
+}
