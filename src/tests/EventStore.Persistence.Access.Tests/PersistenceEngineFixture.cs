50a16196f74e98b1fee85fc20fd9dbc139bd7607
Damian Hickey <dhickey@gmail.com>
2013-06-05 12:01:45 +0100

    Access persistence acceptance tests passing.

---
+++ src/tests/EventStore.Persistence.Access.Tests/PersistenceEngineFixture.cs
@@ -0,0 +1,18 @@
+﻿namespace EventStore.Persistence.AcceptanceTests
+{
+    using Serialization;
+    using SqlPersistence;
+    using SqlPersistence.SqlDialects;
+
+    public partial class PersistenceEngineFixture
+    {
+        public PersistenceEngineFixture()
+        {
+            this.CreatePersistence = () => 
+                new SqlPersistenceFactory(
+                    new ConfigurationConnectionFactory("EventStore.Persistence.AcceptanceTests.Properties.Settings.Access"),
+                    new BinarySerializer(),
+                    new AccessDialect()).Build();
+        }
+    }
+}
