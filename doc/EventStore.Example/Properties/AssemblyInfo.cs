0a7bc55435c525c96f435cbc8214dce69a242f9e
Damian Hickey <dhickey@gmail.com>
2013-06-15 20:57:58 +0100

    Issue #196 delete example project. It's now in it's own repo.

--- doc/EventStore.Example/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("EventStore.Example")]
-[assembly: AssemblyDescription("")]
-[assembly: Guid("6fd621e0-9047-4449-b136-249383936d5c")]
