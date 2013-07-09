0044b3f2e81380973a04b88177a0d638372adaff
Damian Hickey <dhickey@gmail.com>
2013-07-09 23:09:28 +0100

    Fix ProjectReference paths as result of project relocation

---
+++ src/proj/EventStore.Persistence.RavenPersistence/RavenPersistenceWireupExtensions.cs
@@ -0,0 +1,17 @@
+namespace NEventStore
+{
+    public static class RavenPersistenceWireupExtensions
+	{
+		public static RavenPersistenceWireup UsingRavenPersistence(
+			this Wireup wireup, string connectionName)
+		{
+			return new RavenPersistenceWireup(wireup, connectionName);
+		}
+
+        public static RavenPersistenceWireup UsingRavenPersistence(
+            this Wireup wireup)
+        {
+            return new RavenPersistenceWireup(wireup);
+        }
+	}
+}
