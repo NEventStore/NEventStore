575db79ece64af48959f84dddd6e0a958951c41e
Damian Hickey <dhickey@gmail.com>
2013-07-09 23:39:14 +0100

    Mereg Serialization Wireup into serialization

--- src/proj/NEventStore.Serialization.Json.Wireup/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("EventStore.Serialization.Json.Wireup")]
-[assembly: AssemblyDescription("")]
-[assembly: Guid("845cf3c7-c303-473d-8c9c-5a5f491615dd")]
