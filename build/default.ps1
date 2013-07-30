5f3ccdd6227b8e312414f897dba467bb85b5aa40
Damian Hickey <dhickey@gmail.com>
2013-07-30 21:59:18 +0100

    Fix build scripts as result of all the renaming work

--- build/default.ps1
+++ build/default.ps1
@@ -5,9 +5,7 @@ properties {
 	$src_directory = "$base_directory\src"
 	$output_directory = "$base_directory\output"
 	$packages_directory = "$src_directory\packages"
-
-	$sln_file = "$src_directory\EventStore.sln"
-	$keyfile = "$src_directory/EventStore.snk"
+	$sln_file = "$src_directory\NEventStore.sln"
 	$target_config = "Release"
 	$framework_version = "v4.0"
 	$version = "0.0.0.0"
@@ -26,21 +24,16 @@ task default -depends Build
 task Build -depends Clean, UpdateVersion, Compile, Test
 
 task UpdateVersion {
-	
 	$vSplit = $version.Split('.')
-	
 	if($vSplit.Length -ne 4)
 	{
 		throw "Version number is invalid. Must be in the form of 0.0.0.0"
 	}
-
 	$major = $vSplit[0]
 	$minor = $vSplit[1]
-
 	$assemblyFileVersion = $version
 	$assemblyVersion = "$major.$minor.0.0"
-
-	$versionAssemblyInfoFile = "$src_directory/proj/VersionAssemblyInfo.cs"
+	$versionAssemblyInfoFile = "$src_directory/VersionAssemblyInfo.cs"
 	"using System.Reflection;" > $versionAssemblyInfoFile
 	"" >> $versionAssemblyInfoFile
 	"[assembly: AssemblyVersion(""$assemblyVersion"")]" >> $versionAssemblyInfoFile
@@ -49,37 +42,30 @@ task UpdateVersion {
 
 task Compile {
 	exec { msbuild /nologo /verbosity:quiet $sln_file /p:Configuration=$target_config /t:Clean }
-
 	exec { msbuild /nologo /verbosity:quiet $sln_file /p:Configuration=$target_config /p:TargetFrameworkVersion=v4.0 }
 }
 
 task Test -depends RunUnitTests, RunPersistenceTests, RunSerializationTests
 
 task RunUnitTests {
-	write-host "Unit Tests"
-
+	"Unit Tests"
 	EnsureDirectory $output_directory
-
-	Invoke-XUnit -Path $src_directory\tests -TestSpec '*EventStore.Core.Tests.dll' `
+	Invoke-XUnit -Path $src_directory\tests -TestSpec '*NEventStore.Tests.dll' `
     -SummaryPath $output_directory\unit_tests.xml `
     -XUnitPath $xunit_path
 }
 
 task RunPersistenceTests -precondition { $runPersistenceTests } {
-	write-host "Persistence Tests"
-
+	"Persistence Tests"
 	EnsureDirectory $output_directory
-
 	Invoke-XUnit -Path $src_directory\tests -TestSpec '*Persistence.*.Tests.dll' `
     -SummaryPath $output_directory\persistence_tests.xml `
     -XUnitPath $xunit_path
 }
 
 task RunSerializationTests {
-	write-host "Serialization Tests"
-
+	"Serialization Tests"
 	EnsureDirectory $output_directory
-
 	Invoke-XUnit -Path $src_directory\tests -TestSpec '*Serialization.*.Tests.dll' `
     -SummaryPath $output_directory\serialization_tests.xml `
     -XUnitPath $xunit_path
@@ -90,56 +76,27 @@ task Package -depends Build, PackageEventStore, PackageMongoPersistence, Package
 }
 
 task PackageEventStore -depends Clean, Compile {
-	mkdir "$output_directory\bin" | out-null
-	Merge-Assemblies -outputFile "$output_directory\bin\EventStore.dll" -exclude "EventStore.*" -keyfile $keyFile -files @(
-		"$src_directory\proj\EventStore\bin\$target_config\EventStore.dll", 
-		"$src_directory\proj\EventStore.Core\bin\$target_config\EventStore.Core.dll",
-		"$src_directory\proj\EventStore.Serialization\bin\$target_config\EventStore.Serialization.dll",
-		"$src_directory\proj\EventStore.Persistence.SqlPersistence\bin\$target_config\EventStore.Persistence.SqlPersistence.dll",
-		"$src_directory\proj\EventStore.Wireup\bin\$target_config\EventStore.Wireup.dll"
-	)
-	
-	write-host Rereferencing Merged Assembly
-	exec { msbuild /nologo /verbosity:quiet $sln_file /p:Configuration=$target_config /t:Clean }
-	
-	exec { msbuild /nologo /verbosity:quiet $sln_file /p:Configuration=$target_config /p:ILMerged=true /p:TargetFrameworkVersion=v4.0 }
+	mkdir "$publish_directory\bin" | out-null
+	copy "$src_directory\NEventStore\bin\$target_config\NEventStore.???" "$publish_directory\bin"
 }
 
 task PackageMongoPersistence -depends Clean, Compile,PackageEventStore {
-	mkdir $output_directory\plugins\persistence\mongo | out-null
-
-	Merge-Assemblies -outputFile "$output_directory/plugins/persistence/mongo/EventStore.Persistence.MongoPersistence.dll" -exclude "EventStore.*" -keyfile $keyFile -files @(
-		"$src_directory/proj/EventStore.Persistence.MongoPersistence/bin/$target_config/EventStore.Persistence.MongoPersistence.dll",
-		"$src_directory/proj/EventStore.Persistence.MongoPersistence.Wireup/bin/$target_config/EventStore.Persistence.MongoPersistence.Wireup.dll"
-	)
-
-	copy "$src_directory\proj\EventStore.Persistence.MongoPersistence\bin\$target_config\MongoDB*.dll" "$output_directory\plugins\persistence\mongo"
+	mkdir $publish_directory\plugins\persistence\mongo | out-null
+	copy "$src_directory\NEventStore.Persistence.MongoPersistence\bin\$target_config\NEventStore.Persistence.MongoPersistence.???" "$publish_directory\plugins\persistence\mongo"
 }
 
 task PackageRavenPersistence -depends Clean, Compile, PackageEventStore {
-	mkdir $output_directory\plugins\persistence\raven | out-null
-	
-	Merge-Assemblies -outputFile "$output_directory/plugins/persistence/raven/EventStore.Persistence.RavenPersistence.dll" -exclude "EventStore.*" -keyfile $keyFile -files @(
-		"$src_directory/proj/EventStore.Persistence.RavenPersistence/bin/$target_config/EventStore.Persistence.RavenPersistence.dll",
-		"$src_directory/proj/EventStore.Persistence.RavenPersistence.Wireup/bin/$target_config/EventStore.Persistence.RavenPersistence.Wireup.dll"
-	)
-
-	copy "$src_directory\proj\EventStore.Persistence.RavenPersistence\bin\$target_config\Raven*.dll" "$output_directory\plugins\persistence\raven"
+	mkdir $publish_directory\plugins\persistence\raven | out-null
+	copy "$src_directory\NEventStore.Persistence.RavenPersistence\bin\$target_config\NEventStore.Persistence.RavenPersistence.???" "$publish_directory\plugins\persistence\raven"
 }
 
 task PackageJsonSerialization -depends Clean, Compile, PackageEventStore {
-	mkdir $output_directory\plugins\serialization\json-net | out-null
+	mkdir $publish_directory\plugins\serialization\json-net | out-null
 
-	Merge-Assemblies -outputFile "$output_directory/plugins/serialization/json-net/EventStore.Serialization.Json.dll" -exclude "EventStore.*" -keyfile $keyFile -files @(
-		"$src_directory/proj/EventStore.Serialization.Json/bin/$target_config/EventStore.Serialization.Json.dll", 
-		"$src_directory/proj/EventStore.Serialization.Json/bin/$target_config/Newtonsoft.Json*.dll",
-		"$src_directory/proj/EventStore.Serialization.Json.Wireup/bin/$target_config/EventStore.Serialization.Json.Wireup.dll"
+	Merge-Assemblies -outputFile "$publish_directory/plugins/serialization/json-net/NEventStore.Serialization.Json.dll" -exclude "NEventStore.*"  -files @(
+		"$src_directory/NEventStore.Serialization.Json/bin/$target_config/NEventStore.Serialization.Json.dll", 
+		"$src_directory/NEventStore.Serialization.Json/bin/$target_config/Newtonsoft.Json*.dll"
 	)
-}
-
-task PackageDocs {
-	mkdir "$output_directory\doc"
-	copy "$base_directory\doc\*.*" "$output_directory\doc"
 }
 
 task Clean {
