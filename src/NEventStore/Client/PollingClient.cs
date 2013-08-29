0ca91f557a161d6a50d8a1785a2af3473fbfb854
Damian Hickey <dhickey@gmail.com>
2013-08-29 13:31:55 +0100

    Move task helpers to own file.

--- src/NEventStore/Client/PollingClient.cs
+++ src/NEventStore/Client/PollingClient.cs
@@ -69,7 +69,7 @@
                     Dispose();
                     return;
                 }
-                Delay(_interval, _stopRequested.Token)
+                TaskHelpers.Delay(_interval, _stopRequested.Token)
                     .WhenCompleted(
                     _ =>
                         {
@@ -108,81 +108,6 @@
             }
         }
 
-        private static Task Delay(double milliseconds, CancellationToken cancellationToken)
-        {
-            var tcs = new TaskCompletionSource<bool>();
-            var timer = new System.Timers.Timer();
-            timer.Elapsed += (obj, args) => tcs.TrySetResult(true);
-            timer.Interval = milliseconds;
-            timer.AutoReset = false;
-            timer.Start();
-            CancellationTokenRegistration cancellationTokenRegistration = cancellationToken.Register(() =>
-            {
-                timer.Stop();
-                tcs.TrySetCanceled();
-            });
-            return tcs.Task.ContinueWith(_ =>
-            {
-                cancellationTokenRegistration.Dispose();
-                timer.Dispose();
-            },TaskContinuationOptions.ExecuteSynchronously);
-        }
-    }
-
-    internal static class TaskExtensions
-    {
-        public static void WhenCompleted<T>(this Task<T> task, Action<Task<T>> onComplete, Action<Task<T>> onFaulted, bool execSync = false)
-        {
-            if (task.IsCompleted)
-            {
-                if (task.IsFaulted)
-                {
-                    onFaulted.Invoke(task);
-                    return;
-                }
-
-                onComplete.Invoke(task);
-                return;
-            }
-
-            task.ContinueWith(
-                onComplete,
-                execSync ?
-                    TaskContinuationOptions.ExecuteSynchronously | TaskContinuationOptions.OnlyOnRanToCompletion :
-                    TaskContinuationOptions.OnlyOnRanToCompletion);
-
-            task.ContinueWith(
-                onFaulted,
-                execSync ?
-                    TaskContinuationOptions.ExecuteSynchronously | TaskContinuationOptions.OnlyOnFaulted :
-                    TaskContinuationOptions.OnlyOnFaulted);
-        }
-
-        public static void WhenCompleted(this Task task, Action<Task> onComplete, Action<Task> onFaulted, bool execSync = false)
-        {
-            if (task.IsCompleted)
-            {
-                if (task.IsFaulted)
-                {
-                    onFaulted.Invoke(task);
-                    return;
-                }
-
-                onComplete.Invoke(task);
-                return;
-            }
-
-            task.ContinueWith(
-                onComplete,
-                execSync ?
-                    TaskContinuationOptions.ExecuteSynchronously | TaskContinuationOptions.OnlyOnRanToCompletion :
-                    TaskContinuationOptions.OnlyOnRanToCompletion);
-
-            task.ContinueWith(
-                onFaulted,
-                execSync ?
-                    TaskContinuationOptions.ExecuteSynchronously | TaskContinuationOptions.OnlyOnFaulted :
-                    TaskContinuationOptions.OnlyOnFaulted);
-        }
+        
     }
 }
