ee0423d91001f1efe0b16d01003aff11a363d11a
Damian Hickey <dhickey@gmail.com>
2013-09-04 11:39:30 +0100

    Add PollingClientExample

---
+++ src/NEventStore.PollingClientExample/MainProgram.cs
@@ -0,0 +1,68 @@
+namespace NEventStore.PollingClientExample
+{
+    using System;
+    using NEventStore.Client;
+    using NEventStore.Persistence.Sql.SqlDialects;
+
+    internal static class MainProgram
+    {
+        private static readonly byte[] EncryptionKey = {0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf};
+
+        private static void Main()
+        {
+            using (var store = WireupEventStore())
+            {
+                var client = new PollingClient(store.Advanced);
+                string checkpointValue = LoadCheckpoint();
+                ICheckpoint checkpoint = string.IsNullOrEmpty(checkpointValue)
+                    ? store.Advanced.StartCheckpoint
+                    : store.Advanced.ParseCheckpoint(checkpointValue);
+
+                using (IObserveCommits observeCommits = client.ObserveFrom(checkpoint))
+                using (observeCommits.Subscribe(commit =>
+                {
+                    // Project the commit etc
+                    Console.WriteLine(Resources.CommitInfo, commit.BucketId, commit.StreamId, commit.CommitSequence);
+                    // Track the most recent checkpoint
+                    checkpoint = commit.Checkpoint;
+                }))
+                {
+                    observeCommits.Start();
+
+                    Console.WriteLine(Resources.PressAnyKey);
+                    Console.ReadKey();
+
+                    SaveCheckpoint(checkpoint);
+                }
+            }
+        }
+
+        private static string LoadCheckpoint()
+        {
+            // Load the checkpoint value from disk / local db/ etc
+            return null;
+        }
+
+        private static void SaveCheckpoint(ICheckpoint checkpoint)
+        {
+            string checkpointValue = checkpoint.Value;
+            //Save checkpointValue to disk / whatever.
+        }
+
+        private static IStoreEvents WireupEventStore()
+        {
+            return
+                Wireup.Init()
+                    .LogToOutputWindow()
+                    .UsingInMemoryPersistence()
+                    .UsingSqlPersistence("NEventStore") // Connection string is in app.config
+                        .WithDialect(new MsSqlDialect()).EnlistInAmbientTransaction() // two-phase commit
+                        .InitializeStorageEngine()
+                        .TrackPerformanceInstance("example")
+                        .UsingJsonSerialization()
+                        .Compress()
+                        .EncryptWith(EncryptionKey)
+                    .Build();
+        }
+    }
+}
