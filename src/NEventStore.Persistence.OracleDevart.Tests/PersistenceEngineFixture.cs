e1cf327b01592d5255e6c2d0baf124cb3828f774
christophdebaene <christoph.debaene@gmail.com>
2013-12-02 13:01:15 +0100

    Added Oracle Unit Test Project for Devart

---
+++ src/NEventStore.Persistence.OracleDevart.Tests/PersistenceEngineFixture.cs
@@ -0,0 +1,19 @@
+﻿namespace NEventStore.Persistence.AcceptanceTests
+{
+    using NEventStore.Persistence.Sql;
+    using NEventStore.Persistence.Sql.SqlDialects;
+    using NEventStore.Serialization;
+
+    public partial class PersistenceEngineFixture
+    {
+        public PersistenceEngineFixture()
+        {
+            _createPersistence = pageSize =>
+                new SqlPersistenceFactory(
+                    new EnviromentConnectionFactory("Oracle", "Devart.Data.Oracle"),
+                    new BinarySerializer(),
+                    new OracleNativeDialect(),
+                    pageSize: pageSize).Build();
+        }
+    }
+}
