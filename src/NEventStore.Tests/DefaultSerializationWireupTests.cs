f468776dba422158bfbfce6f49accfdc896bdc96
pgermishuys <pieter@pieterg.com>
2013-12-29 21:28:39 +0200

    Failing test for a sql persistence wireup without an explicit serializer

---
+++ src/NEventStore.Tests/DefaultSerializationWireupTests.cs
@@ -0,0 +1,41 @@
+﻿namespace NEventStore
+{
+	using NEventStore.Persistence.AcceptanceTests;
+	using NEventStore.Persistence.AcceptanceTests.BDD;
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using Xunit;
+	using Xunit.Should;
+
+	public class DefaultSerializationWireupTests
+	{
+		public class when_building_an_event_store_without_an_explicit_serializer : SpecificationBase
+		{
+			private Wireup wireup;
+			private Exception exception;
+			protected override void Context()
+			{
+				wireup = Wireup.Init()
+					.UsingSqlPersistence("fakeConnectionString")
+						.WithDialect(new Persistence.Sql.SqlDialects.MsSqlDialect());
+			}
+
+			protected override void Because()
+			{
+				exception = Catch.Exception(() => { wireup.Build(); });
+			}
+
+			protected override void Cleanup()
+			{
+			}
+
+			[Fact]
+			public void should_not_throw_an_argument_null_exception()
+			{
+				exception.ShouldNotBeInstanceOf<ArgumentNullException>();
+			}
+		}
+	}
+}
