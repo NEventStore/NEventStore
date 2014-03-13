1a9b1d8fc687d68e7d8a22ff373009e06a58ad7e
Damian Hickey <dhickey@gmail.com>
2014-03-13 19:54:21 +0100

    Add OnPurge and OnDeleteStream to IPipelineHook. Add PipelineHookBase that provided optional default implementation of IPipelineHook.

--- src/NEventStore/IPipelineHook.cs
+++ src/NEventStore/IPipelineHook.cs
@@ -29,5 +29,18 @@ namespace NEventStore
         /// </summary>
         /// <param name="committed">The commit which has been persisted.</param>
         void PostCommit(ICommit committed);
+
+        /// <summary>
+        ///     Invoked when a bucket has been purged. If buckedId is null, then all buckets have been purged.
+        /// </summary>
+        /// <param name="bucketId">The bucket Id that has been purged. Null when all buckets have been purged.</param>
+        void OnPurge(string bucketId);
+
+        /// <summary>
+        ///     Invoked when a stream has been deleted.
+        /// </summary>
+        /// <param name="bucketId">The bucket Id from which the stream whch has been deleted.</param>
+        /// <param name="streamId">The stream Id of the stream which has been deleted.</param>
+        void OnDeleteStream(string bucketId, string streamId);
     }
 }
