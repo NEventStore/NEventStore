112b6f72ae56f3a85b300aa0376ab48a55f8a41b
Sergei Almazov <almazik@gmail.com>
2014-07-21 13:57:41 +0300

    Added typed conflict resolution delegate and corresponding processing, marked untyped delegate and methods as obsolete.

--- src/NEventStore/CommonDomain/IDetectConflicts.cs
+++ src/NEventStore/CommonDomain/IDetectConflicts.cs
@@ -1,13 +1,36 @@
 namespace CommonDomain
 {
+	using System;
 	using System.Collections.Generic;
 
 	public interface IDetectConflicts
 	{
-		void Register<TUncommitted, TCommitted>(ConflictDelegate handler) where TUncommitted : class where TCommitted : class;
+		void Register<TUncommitted, TCommitted>(ConflictDelegate<TUncommitted, TCommitted> handler)
+			where TUncommitted : class
+			where TCommitted : class;
 
 		bool ConflictsWith(IEnumerable<object> uncommittedEvents, IEnumerable<object> committedEvents);
 	}
 
+	[Obsolete("Use ConflictDelegate<TUncommitted, TCommitted> delegate instead")]
 	public delegate bool ConflictDelegate(object uncommitted, object committed);
+
+	public delegate bool ConflictDelegate<in TUncommitted, in TCommitted>(TUncommitted uncommitted, TCommitted committed)
+		where TUncommitted : class
+		where TCommitted : class;
+
+	public static class DetectConflictsExtensions
+	{
+		/// <summary>
+		///   Provides backward compatibility for untyped ConflictDelegate users
+		/// </summary>
+		// ReSharper disable once CSharpWarnings::CS0612
+		[Obsolete("Use Register<TUncommitted, TCommitted>(ConflictDelegate<TUncommitted, TCommitted>) overload instead")]
+		public static void Register<TUncommitted, TCommitted>(this IDetectConflicts conflictDetector, ConflictDelegate handler)
+			where TUncommitted : class
+			where TCommitted : class
+		{
+			conflictDetector.Register<TUncommitted, TCommitted>((uncommitted, committed) => handler(uncommitted, committed));
+		}
+	}
 }
