72023606ff239d07092f661d5cba6cb4b7267b30
Brian Surowiec <xtorted@optonline.net>
2014-08-18 16:45:39 -0400

    Fix typo in exception message

--- src/NEventStore/CommonDomain/Core/ExtensionMethods.cs
+++ src/NEventStore/CommonDomain/Core/ExtensionMethods.cs
@@ -12,7 +12,7 @@ namespace CommonDomain.Core
 		public static void ThrowHandlerNotFound(this IAggregate aggregate, object eventMessage)
 		{
 			string exceptionMessage =
-				"Aggregate of type '{0}' raised an event of type '{1}' but not handler could be found to handle the message."
+				"Aggregate of type '{0}' raised an event of type '{1}' but no handler could be found to handle the message."
 					.FormatWith(aggregate.GetType().Name, eventMessage.GetType().Name);
 
 			throw new HandlerForDomainEventNotFoundException(exceptionMessage);
