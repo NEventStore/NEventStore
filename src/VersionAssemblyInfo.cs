74fafdd719fe6abefeebc0bbfb12fa951b6bebd2
Alkampfergit <ricci.gianmaria@gmail.com>
2016-06-14 17:46:11 +0200

    Removed versioning from VersionAssemblyInfo.cs
    This is needed to comply with the new build
    script that uses gitversion. GitVersion uses
    to update AssemblyInfo.cs directly, without
    needing project to reference a single VersionAssemblyInfo.cs.

--- src/VersionAssemblyInfo.cs
+++ src/VersionAssemblyInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("5.2.0.0")]
-[assembly: AssemblyFileVersion("5.2.0.0")]
+//[assembly: AssemblyVersion("5.2.0.0")]
+//[assembly: AssemblyFileVersion("5.2.0.0")]
