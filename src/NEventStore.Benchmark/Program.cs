f8b0c41eea2af8529db45cb4da1c08f1024a9d71
Alessandro Giorgetti <alessandro.giorgetti@live.com>
2019-03-07 11:05:41 +0100

    Added a Benchmark project

---
+++ src/NEventStore.Benchmark/Program.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Running;
+using NEventStore.Benchmark.Benchmarks;
+using System;
+
+namespace NEventStore.Benchmark
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<Md5VsSha256>();         
+        }
+    }
+}
