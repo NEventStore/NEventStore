5666fde8f97d1f192c585a67267e274cd26293e3
Piergiorgio Vagnozzi <piergiorgio_vagnozzi@hotmail.com>
2024-11-30 16:39:16 +0100

    Clean

--- src/NEventStore.Persistence.AcceptanceTests/Properties/AssemblyInfo.cs
+++ src/NEventStore.Persistence.AcceptanceTests/Properties/AssemblyInfo.cs
@@ -41,4 +41,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("9.1.0.0")]
 [assembly: AssemblyFileVersion("9.1.0.0")]
 [assembly:
-    AssemblyInformationalVersion("9.1.0-issues-net9.8+Branch.issues-net9.Sha.8aac645da16a4e0dbab16c6d64b3d076cfa6e111")]
+    AssemblyInformationalVersion("9.1.0-issues-net9.11+Branch.issues-net9.Sha.153742f3c054fabc8c14b612df2869cabeceaf5b")]
