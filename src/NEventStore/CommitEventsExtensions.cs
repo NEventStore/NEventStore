2548cc5623d2811ca28b6218a3589514661892a3
Alessandro Giorgetti <alessandro.giorgetti@live.com>
2025-01-02 19:09:13 +0100

    WIP: Deprecate DateTime methods; add async snapshot support
    Mark `GetFromTo` and `GetFrom` as obsolete in favor of
    new methods using `Int64` checkpoint tokens. Introduce
    `GetSnapshot` and `GetStreamsToSnapshot` methods, along
    with their asynchronous counterparts. Create the
    `IAccessSnapshotsAsync` interface for thread-safe
    snapshot operations and update `IPersistStreams` to
    include it. Update `InMemoryPersistenceEngine` and
    `PipelineHooksAwarePersistStreamsDecorator` to support
    the new asynchronous functionality.

--- src/NEventStore/CommitEventsExtensions.cs
+++ src/NEventStore/CommitEventsExtensions.cs
@@ -3,7 +3,7 @@ using NEventStore.Persistence;
 namespace NEventStore
 {
     /// <summary>
-    ///    Provides extension methods for <see cref="ICommitEvents"/> instances.
+    ///    Provides extension methods for <see cref="ICommitEvents"/> and <see cref="ICommitEventsAsync"/> instances.
     /// </summary>
     public static class CommitEventsExtensions
     {
@@ -32,7 +32,7 @@ namespace NEventStore
         ///     Gets the corresponding commits from the stream indicated starting at the revision specified until the
         ///     end of the stream sorted in ascending order--from oldest to newest from the default bucket.
         /// </summary>
-        /// <param name="commitEvents">The <see cref="ICommitEvents"/> instance.</param>
+        /// <param name="commitEvents">The <see cref="ICommitEventsAsync"/> instance.</param>
         /// <param name="streamId">The stream from which the events will be read.</param>
         /// <param name="minRevision">The minimum revision of the stream to be read.</param>
         /// <param name="maxRevision">The maximum revision of the stream to be read.</param>
